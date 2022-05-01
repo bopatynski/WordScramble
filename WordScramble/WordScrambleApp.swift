540bb786f30d08cdacabea7b7cfba727bc7a1bd0
bopatynski <bopatynski@gmail.com>
2022-05-01 17:26:22 -0400

    Initial Commit

---
+++ WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Bogdan Patynski on 2022-05-01.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
