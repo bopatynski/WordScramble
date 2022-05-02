e3867453e72c792af2b0da64b70c2a025654a2ba
bopatynski <bopatynski@gmail.com>
2022-05-02 00:10:08 -0400

    Pretty much done"
    Have to add disallow answers shorter than 3 letter or OG word
    
    Add tool bar to call start game
    
    Put a text view to calculate score + score calculation

--- WordScramble/ContentView.swift
+++ WordScramble/ContentView.swift
@@ -8,9 +8,114 @@
 import SwiftUI
 
 struct ContentView: View {
+    func startGame() {
+        if let fileURL = Bundle.main.url(forResource: "start", withExtension: "txt") {
+              // load start txt into a string
+            if let startWords = try? String(contentsOf: fileURL){
+                let allWords = startWords.components(separatedBy: "\n")
+                
+                rootWord = allWords.randomElement() ?? "silkword"
+                
+                return
+            }
+        }
+        fatalError("Could not load start file from bundle")
+        
+    }       
+    @State private var usedWords = [String]()
+    @State private var rootWord = ""
+    @State private var newWord = ""
+    
+    func addNewWord(){
+        // lowercase and trim the word so we don't add dupes
+        let answer = newWord.lowercased().trimmingCharacters(in: .whitespacesAndNewlines)
+        // exit if remaining string is empty
+        
+        guard answer.count > 0 else { return }
+        
+        guard isOriginal(word: answer) else {
+            wordError(title: "Word already used", message: "be more original")
+            return
+        }
+        
+        guard isReal(word: answer) else {
+            wordError(title: "Word not possible", message: "You can't spell that here")
+            return
+        }
+        
+        guard isPossible(word: answer) else {
+            wordError(title: "Word not recognized", message: "You can't make up words fool")
+            return
+        }
+        
+        withAnimation {
+            usedWords.insert(answer, at: 0)
+        }
+        newWord = ""
+    }
+    func isOriginal(word: String) -> Bool {
+        !usedWords.contains(word)
+    }
+    
+    func isPossible(word:String) -> Bool {
+        var tempWord = rootWord
+        
+        for letter in word {
+            if let pos = tempWord.firstIndex(of: letter) {
+                tempWord.remove(at: pos)
+            } else {
+                return false
+            }
+        }
+        return true
+    }
+    
+    func isReal(word: String) -> Bool {
+        let checker = UITextChecker()
+        
+        let range = NSRange(location: 0, length: word.utf16.count)
+        let mispelledRange = checker.rangeOfMisspelledWord(in: word, range: range, startingAt: 0, wrap: false, language: "en")
+        
+        return mispelledRange.location == NSNotFound
+    }
+    
+    @State private var errorTitle = ""
+    @State private var errorMessage = ""
+    @State private var showringError = false
+    
+    func wordError(title: String, message: String){
+        errorTitle = title
+        errorMessage = message
+        showringError = true
+    }
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView{
+            List{
+                Section{
+                    TextField("Enter your word", text: $newWord)
+                        .autocapitalization(.none)
+                }
+                Section{
+                    ForEach(usedWords, id: \.self) { word in
+                        HStack{
+                            Image(systemName: "\(word.count).circle")
+                            Text(word)
+                        }
+                    }
+                }
+
+                
+            }
+            .navigationTitle(rootWord)
+            .onSubmit(addNewWord)
+            .onAppear(perform: startGame)
+            .alert(errorTitle, isPresented: $showringError){
+                Button("OK", role: .cancel) { }
+            } message: {
+                Text(errorMessage)
+            }
+        }
     }
 }
 
